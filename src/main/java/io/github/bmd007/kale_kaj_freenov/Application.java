25ecd72304162b0ee24623a0ff2350315b8b824d
Mahdi <bmd579@gmail.com>
2025-10-04 13:06:07 +0200

    abc

--- src/main/java/io/github/bmd007/kale_kaj_freenov/Application.java
+++ src/main/java/io/github/bmd007/kale_kaj_freenov/Application.java
@@ -54,10 +54,11 @@ public class Application {
         pca9685.writeRegister(MODE1, (byte) 0xA1);
         this.pca9685 = pca9685;
 
-        piCamera.setWidth(500).setHeight(500) // Set Camera to produce 500x500 images.
+        piCamera.setWidth(500)
+            .setHeight(500) // Set Camera to produce 500x500 images.
             .setBrightness(75)                // Adjust Camera's brightness setting.
             .setExposure(Exposure.AUTO)       // Set Camera's exposure.
-            .setTimeout(2)                    // Set Camera's timeout.
+            .setTimeout(2)// Set Camera's timeout.
             .setAddRawBayer(true);            // Add Raw Bayer data to image files created by Camera.
 // Sets all Camera options to their default settings, overriding any changes previously made.
         piCamera.setToDefaults();
