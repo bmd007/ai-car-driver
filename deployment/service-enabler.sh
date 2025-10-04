ed65a8c08f8ed828d87ad2f6975551bddb69763a
Mahdi <bmd579@gmail.com>
2025-10-04 23:35:26 +0200

    abc

--- deployment/service-enabler.sh
+++ deployment/service-enabler.sh
@@ -1,3 +1,10 @@
+sudo systemctl stop kale-kaj || true
+sudo systemctl disable kale-kaj || true
+sudo pkill -f kale-kaj || true
+
+sudo kill -9 $(sudo lsof -ti:8080) 2>/dev/null || true
+sudo rm -f /etc/systemd/system/kale-kaj.service
+
 sudo cp kale-kaj.service /etc/systemd/system/kale-kaj.service
 sudo systemctl daemon-reload
 sudo systemctl enable kale-kaj
