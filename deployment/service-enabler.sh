9902864b6b60f880d45aefc4e28846d356f94eda
Mahdi <bmd579@gmail.com>
2025-10-04 23:26:27 +0200

    abc

--- deployment/service-enabler.sh
+++ deployment/service-enabler.sh
@@ -1,4 +1,4 @@
-cp kale-kaj.service /etc/systemd/system/kale-kaj.service
+sudo cp kale-kaj.service /etc/systemd/system/kale-kaj.service
 sudo systemctl daemon-reload
 sudo systemctl enable kale-kaj
 sudo systemctl start kale-kaj
